hotfix: fix image frequency calculation

mnist_dcgan.py
@@ -41,7 +41,8 @@ dataloader = torch.utils.data.DataLoader(mnist,
                                          shuffle=True)
 
 
-image_freq = math.ceil(args["epochs"] / 50)
+total_batch_per_epoch = math.ceil(len(mnist) / args["batch_size"])
+image_freq = math.ceil(args["epochs"] * total_batch_per_epoch / 50)
 
 if args['logger_type'] == 'console':
     logger = ConsoleLogger(__name__) 